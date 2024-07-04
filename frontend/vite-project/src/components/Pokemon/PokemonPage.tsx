ef6d20316339d99589aa7e4729ce46d4b32786c3
TimedT <PasangSherpa10402@gmail.com>
2024-07-04 14:50:22 -0700

    pokemon page needs css

--- frontend/vite-project/src/components/Pokemon/PokemonPage.tsx
+++ frontend/vite-project/src/components/Pokemon/PokemonPage.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from "react";
-
+import "../../styles/PokemonPage.css"
 
 
 const BASE_URL = "http:localhost:5000";
 const URL = "https://jsonplaceholder.typicode.com/todos/1";
 
 
-interface Pokemon {
-    name: string;
+interface Ability {
     id: number;
+    name: string;
+    effect: string;
+}
+
+interface Type {
+    id: number;
+    name: string;
+}
+
+interface Stats {
     hp: number;
     attack: number;
     defense: number;
@@ -17,14 +26,26 @@ interface Pokemon {
     speed: number;
     height: number;
     weight: number;
+}
+
+interface Pokemon {
+    name: string;
+    abilities: Ability[];
+    id: number;
+    hp: number;
+    type: Type[];
+    stats: Stats;
+    height: number;
+    weight: number;
     image: string;
 }
 
 
 function PokemonPage() {
     const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-    // const [ability, setAbility] = useState([]);
-    // const [abilities, setAbilities] = useState([]);
+    const [abilities, setAbilities] = useState<Ability[]>([]);
+    const [type, setType] = useState<Type[]>([]);
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -32,20 +53,13 @@ function PokemonPage() {
                 const response = await fetch(`http://127.0.0.1:5000/api/pokemon/ditto`);
                 if (!response.ok) {
                     throw new Error("Failed to fetch");
-                } else {
-                    console.log("response does works");
                 }
                 const data = await response.json();
-                if (data == null) {
-                    console.log("call works");
-                }
-                console.log("something else");
-                console.log(data);
                 setPokemon(data);
-                // setAbility(data.abilities[1].ability)
-                // setAbilities(data.abilities[0].ability)
+                setAbilities(data.abilities);
+                setType(data.type);
             } catch (error) {
-                console.error("API request failed. Using mock data instead:", error);
+                console.error("API request failed", error);
             }
         };
         fetchData();
@@ -53,11 +67,34 @@ function PokemonPage() {
 
     return (
         <>
-            <div>
+            <div className="basic-info-container">
                 <h1>ditto: {pokemon?.name}</h1>
-                <ul>{ }
+                <ul>Type:
+                    {/* {type.map((t, index) => (
+                        <li className="no-bullets" key={type.id | index}>
+                            <a href={`/type/${type.name}`} > {type.name}</a>
+                        </li>
+                    ))} */}
+                </ul>
+                <div>Abilities</div>
+                {abilities.map((ability, index) => (
+                    <li className="no-bullets" key={ability.id | index}>
+                        {index + 1}. <a href={`/ability/${ability.name}`} > {ability.name}</a>
+                    </li>
+                ))}
+            </div >
+            <div className="base-stats-container">
+                <h1>BASE STATS</h1>
+                <ul className="no-bullets">
+                    <li>HP: {pokemon?.stats.hp}</li>
+                    <li>Atack: {pokemon?.stats.attack}</li>
+                    <li>Defense: {pokemon?.stats.defense}</li>
+                    <li>Sp. Atk: {pokemon?.stats.specialAttack}</li>
+                    <li>Sp. Def: {pokemon?.stats.specialDefense}</li>
+                    <li>Speed: {pokemon?.stats.speed}</li>
                 </ul>
             </div>
+
         </>
     );
 }
