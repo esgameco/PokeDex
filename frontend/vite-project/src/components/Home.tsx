f754b1dd94daf44d0a56c728f2c8444fb8326f37
TimedT <PasangSherpa10402@gmail.com>
2024-07-07 00:50:06 -0700

    home page css

--- frontend/vite-project/src/components/Home.tsx
+++ frontend/vite-project/src/components/Home.tsx
@@ -1,11 +1,11 @@
-
+import "../styles/Pokemon.css"
 
 function Home() {
     let things = ["pokemon things", "mabye pokemon things"]
 
     if (things) {
         return (
-            <>
+            <div className="container">
                 <h1>Pokedex</h1>
                 <ul className="list-group">
                     {things.map((things) => (
@@ -13,7 +13,7 @@ function Home() {
                     ))}
                 </ul>
                 <a href="/Pokemon/incineroar">Incineorar</a>
-            </>
+            </div>
         );
     } else {
         return <><h1>things</h1></>
